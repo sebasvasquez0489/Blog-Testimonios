a1044a85a02d3643bcd50db3b415bb446d76776d
Sebastian Vasquez <sebitas0489@gmail.com>
2024-01-17 19:46:48 -0500

    Agregando PROPS.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Testimonio from "./componentes/Testimonio";
 
 function App() {
   return (
     <div className="App">
-
+      <div className="contenedor-principal">
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+      </div>
+      <Testimonio
+        nombre="Emma Bostian"
+        pais="Suecia"
+        imagen="Emma"
+        cargo="Ingeniero de software"
+        empresa="Spotify"
+        testimonio="Siempre me ha costado aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."
+      />
     </div>
   );
 }
