8a693b7a921272d4989855deb48d518718e1be6f
Sebastian Vasquez <sebitas0489@gmail.com>
2024-01-17 20:07:48 -0500

    Se insertan testimonios, fin de proyecto.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,22 @@ function App() {
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
       </div>
       <Testimonio
+        nombre="Shawn Wang"
+        pais="Singapur"
+        imagen="Shawn"
+        cargo="Ingeniero de software"
+        empresa="Amazon"
+        testimonio="Da miedo cambiar de carrera. Solo gané confianza en que podía codificar trabajando durante cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida. "
+      />
+      <Testimonio
+        nombre="Sara Chima"
+        pais="Nigeria"
+        imagen="Sarah"
+        cargo="Ingeniero de software"
+        empresa="ChatDesk"
+        testimonio="freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de codificación desde un nivel principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollo en una empresa increíble"
+      />
+      <Testimonio
         nombre="Emma Bostian"
         pais="Suecia"
         imagen="Emma"
